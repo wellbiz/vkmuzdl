20f11676ee937db7256bfceab5bbf2455279dff9
Anton <anton9121@gmail.com>
2013-01-25 14:28:54 +0600

    0.1.4

--- js/vkmp3.js
+++ js/vkmp3.js
@@ -1,4 +1,4 @@
-$('body > div').eq(0).parent().append('<style>div.get_it_btn {position: absolute; background: #FFF; z-index: 500; display: none; height: 16px; padding-left: 3px; padding-right: 3px;} div.get_it_btn:hover{background-color: #EDF1F5;} div.det_it_btn a{color: #2B587A}</style>');
+$('body > div').eq(0).parent().append('<style>div.get_it_btn {position: absolute; background: #FFF; z-index: 500; display: none; height: 16px; padding-left: 3px; padding-right: 3px;} div.get_it_btn:hover{background-color: #EDF1F5;} div.get_it_btn a{color: #2B587A}</style>');
 $('body > div').eq(0).parent().append('<div class="get_it_btn"><a>Download</a></div>');
 var _get_it_btn = $('div.get_it_btn');
 $(document).on('mouseenter', 'div.play_new', function(event) {
@@ -12,7 +12,7 @@ $(document).on('mouseenter', 'div.play_new', function(event) {
             'display':'block'
         }).children('a').attr({
             'href':url.replace(/(.*),[0-9]*/,"$1"), 
-            'download':title+'.mp3'
+            'download':title.replace(/(.*) \([^)(]*\)$/,"$1").trim()+'.mp3'
         });
     }
 }).on('mouseleave', 'div.play_new', function(event) {
